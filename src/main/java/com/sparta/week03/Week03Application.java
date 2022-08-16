3aedbcac8154c0825ae350e3fa06859248ed6b47
sejin\admin <seijin0722@naver.com>
2022-08-16 12:33:23 +0900

    타임스탬프쓸때 꼭 필요한 annoation #1

--- src/main/java/com/sparta/week03/Week03Application.java
+++ src/main/java/com/sparta/week03/Week03Application.java
@@ -2,7 +2,10 @@ package com.sparta.week03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing : JPA를사용하면 꼭 넣어줘야함, 데이터 변동이있어면 알아서 반영해줘
+@EnableJpaAuditing
 @SpringBootApplication
 public class Week03Application {
 
